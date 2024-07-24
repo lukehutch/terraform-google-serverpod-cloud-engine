d9ca563fe3bc6a8f75c8e47cde3836557900f207
Luke Hutchison <luke.hutch@gmail.com>
2024-07-23 19:42:28 -0600

    More fixes

--- variables.tf
+++ variables.tf
@@ -28,7 +28,10 @@ variable "top_domain" {
 
 variable "authorized_networks" {
   description = "The networks authorized to connect to the database from outside the Serverpod infrastructure."
-  type        = map(string)
+  type        = map(object({
+    name  = string
+    value = string
+  }))
   default     = null
 }
 
